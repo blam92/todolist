7048b322f796fe948e878dcec6cfab4710ddcdcd
Santiago Iraola <iraola.santi@gmail.com>
2018-03-01 10:47:58 -0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+let SRC_DIR = path.join(__dirname, '/client/src');
+let DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['react', 'es2015']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015']
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
